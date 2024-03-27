11d886cca835aaddd8af08c67b6f835e2486a729
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-03-27 13:27:39 +0530

    feat: Add CSS styles for page layout and TodoItem component

--- React Todo App/src/App.jsx
+++ React Todo App/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useState } from 'react'
 import TodoItem from './TodoItem'
+import './style.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
 
-    <div>
-     
+    <div className='todo-item'>
     <fieldset>
       <legend>Choose your tasks:</legend>
 
@@ -16,8 +16,7 @@ function App() {
       <TodoItem/>
       <TodoItem/>
       <TodoItem/>
-    
-      
+     
     </fieldset>
     </div>
   )
