c791997540725f7aa966860b7fa3a361f9c2e3dd
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-03-30 23:21:50 +0530

    feat: Add todoList array with task objects

---
+++ React Todo App/src/todosData.jsx
@@ -0,0 +1,20 @@
+const todoList = [
+    {
+        id: 1,
+        text: "Complete assignment",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Buy groceries",
+        completed: true
+    },
+    {
+        id: 3,
+        text: "Go for a run",
+        completed: false
+    }
+];
+
+console.log(todoList);
+export default todoList;
