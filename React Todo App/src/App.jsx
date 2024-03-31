9f9c5c14826aee63320db97f369171391c8e44af
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-03-31 13:23:11 +0530

    feat: Render todo items in App component
    Completed the App component by rendering todo items from the todoList array. Utilized the TodoItem component to display each task within a fieldset. This enhancement integrates the todo functionality into the application, allowing users to view their tasks.

--- React Todo App/src/App.jsx
+++ React Todo App/src/App.jsx
@@ -2,20 +2,25 @@ import React from 'react'
 import { useState } from 'react'
 import TodoItem from './TodoItem'
 import './style.css'
+import todoList from './todosData'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  
   return (
 
     <div className='todo-item'>
     <fieldset>
       <legend>Choose your tasks:</legend>
+      
+      {todoList.map(todo => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
 
+      {/* <TodoItem/>
       <TodoItem/>
       <TodoItem/>
-      <TodoItem/>
-      <TodoItem/>
+      <TodoItem/> */}
      
     </fieldset>
     </div>
--- React Todo App/src/TodoItem.jsx
+++ React Todo App/src/TodoItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './style.css'
+import todoList from "./todosData";
 
 function TodoItem(){
     return(
