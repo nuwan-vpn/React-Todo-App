80ba848fa2834d5ec8f6d5f9dbccefd8b7e3d26f
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-04-07 22:50:04 +0530

    feat: Add completedStyle object and apply conditional styling in TodoItem component

--- React Todo App/src/TodoItem.jsx
+++ React Todo App/src/TodoItem.jsx
@@ -3,6 +3,10 @@ import './style.css'
 import todoList from "./todosData";
 
 function TodoItem(props){
+  const completedStyle = {
+    fontStyle : "italic",
+    color : "#cdcdcd"
+  }
     return(
         <div className="todo-item">
           <input 
@@ -10,7 +14,7 @@ function TodoItem(props){
             checked={props.item.completed}  
             onChange={(Event)=>props.handleChamge(props.item.id)}
           />
-          <p>{props.item.text}</p>
+          <p style={props.item.completed ? completedStyle:null}>{props.item.text}</p>
         </div>
     )
 }
